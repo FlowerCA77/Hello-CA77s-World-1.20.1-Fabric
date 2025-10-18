24d32fc97498d8cd18000f97f76a9e0c8f5f0d24
XhernoPhys CA77 <Ca538468142@outlook.com>
2025-10-18 22:59:27 +0800

    Initial commit

---
+++ src/client/java/com/flowerca77/hello_77_world/HelloCA77sWorldClient.java
@@ -0,0 +1,10 @@
+package com.flowerca77.hello_77_world;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HelloCA77sWorldClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/flowerca77/hello_77_world/HelloCA77sWorldDataGenerator.java
@@ -0,0 +1,11 @@
+package com.flowerca77.hello_77_world;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HelloCA77sWorldDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
